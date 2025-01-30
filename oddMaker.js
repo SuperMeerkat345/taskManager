7e807ce2f932b91713a37a3c6c07ad9c15ac5aff
SuperMeerkat362 <artiom22903@gmail.com>
2025-01-29 22:49:06 -0500

    added oddMaker function which makes an efficient method to calculate whether a number is odd

---
+++ oddMaker.js
@@ -0,0 +1,35 @@
+import * as fs from "node:fs/promises"
+
+async function makeOddChecker (n) {
+    let code = 
+    `// Returns true if number is odd, returns false if not (assuming num is a 15 bit number)
+     
+function isOdd (num) {\n`;
+    
+    for (let i = -1*n; i <= n; i += 2) {
+        if (i == -1*n) {
+            code += 
+`    if (num == ${i}) {
+        return false;
+    }`;
+        }
+        else { 
+        code += 
+`    else if (num == ${i}) {
+        return false;
+    }`;
+        }
+    }
+
+    code += 
+`\n\n    return true;
+}`
+
+    await fs.writeFile("./js/odd.js", code, (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
+}
+
+makeOddChecker(32766);
